1e768b660bf155bbc5b913861b906b1324057647
hoseon <chs1025@gmail.com>
2022-12-27 07:52:34 +0900

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by HoSeon Chu on 2022/12/27.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
